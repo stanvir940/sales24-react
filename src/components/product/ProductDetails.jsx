a6d3b080f4b32257cc16279d8be0c42d474bf39c
stanvir940 <hmtanvir940@gmail.com>
2024-05-08 23:32:05 +0600

    product details page updated

--- src/components/product/ProductDetails.jsx
+++ src/components/product/ProductDetails.jsx
@@ -16,67 +16,54 @@ const ProductDetails = () => {
       .finally(() => setLoading(false));
   }, [id]);
 
+  //console.log(product);
+
   if (loading) return <div>Loading...</div>;
   if (error) return <div>Error: {error}</div>;
   if (!product) return <div>No Product Found</div>;
 
-  const { title } = product;
+  const { title, price, description } = product;
   return (
     <div>
-      <div className=" bg-slate-300">
+      <div className=" bg-white w-90 p-5">
         <div className="grid grid-cols-2 gap-10 bg-white shadow-slate-700">
-          <div className="">
+          <div className="w-85">
             <img
-              src="https://unsplash.com/photos/woman-with-dslr-camera-e616t35Vbeg"
+              src="https://images.pexels.com/photos/2085118/pexels-photo-2085118.jpeg?cs=srgb&dl=pexels-jansel-ferma-306295-2085118.jpg&fm=jpg"
               alt=""
             />
           </div>
-          <p>{title}</p>
-          <label className="swap swap-flip text-9xl">
-            {/* this hidden checkbox controls the state */}
-            <input type="checkbox" />
 
-            <div className="swap-on">😈</div>
-            <div className="swap-off">😇</div>
-          </label>
-          <div className="join">
-            <input
-              className="join-item btn btn-square"
-              type="radio"
-              name="options"
-              aria-label="1"
-              checked
-            />
-            <input
-              className="join-item btn btn-square"
-              type="radio"
-              name="options"
-              aria-label="2"
-            />
-            <input
-              className="join-item btn btn-square"
-              type="radio"
-              name="options"
-              aria-label="3"
-            />
-            <input
-              className="join-item btn btn-square"
-              type="radio"
-              name="options"
-              aria-label="4"
-            />
-          </div>
-          {/* <div>{price}</div> */}
-          <div className="grid grid-cols-2 gap-2">
-            <div>
-              <button className="btn btn-success">Success</button>
+          <div>
+            <p className="text-3xl font-bold text-gray-900">{title}</p>
+            <h3 className=" mt-5">Thoughts on this product</h3>
+            <label className="swap swap-flip text-6xl mt-5 mb-5">
+              {/* this hidden checkbox controls the state */}
+
+              <input type="checkbox" />
+
+              <div className="swap-on">😈</div>
+              <div className="swap-off">😇</div>
+            </label>
+            <p className="text-2xl font-bold text-green-400 mt-5 mb-5">
+              ${price}{" "}
+            </p>
+
+            <div className="grid grid-cols-2 gap-2">
+              <div>
+                <button className="btn btn-success">Buy Now</button>
+              </div>
+              <div>
+                <button className="btn btn-warning">Add to Cart</button>
+              </div>
             </div>
-            <div>
-              <button className="btn btn-warning">Warning</button>
+            <div className="border-t-2 border-gray-300 mt-5"></div>
+
+            <div className="text-2xl font-medium mt-5 mb-5 text-black">
+              {description}
             </div>
           </div>
         </div>
-        {/* <div>{description}</div> */}
       </div>
     </div>
   );
